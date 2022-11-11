b703fd12e9a61a7560e956702031011cd27888cf
Lisa <lisahongofficial@gmail.com>
2022-11-11 16:43:16 -0500

    added dotenv

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from resources.posts import posts
 from resources.user import user
 from resources.comments import comments
 from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from flask_cors import CORS
 
 DEBUG = True
-PORT = 8000
+PORT = os.environ.get("PORT")
 
 app = Flask(__name__)
 
 CORS(user, origins=['http://localhost:3000'], supports_credentials=True)
 
-app.secret_key="LJAKLJLKJJLJKLSDJLKJASD"
+app.secret_key=os.environ.get("APP_SECRET")
 login_manager = LoginManager()
 
 login_manager.init_app(app)
